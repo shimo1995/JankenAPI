9dc4be96051d5f1946aee423e22f82254fb560b1
Shimo <masatoshimojo1995@gmail.com>
2023-06-25 06:06:20 +0900

    add janken function

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -20,24 +20,43 @@ public class Function {
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
      * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
-    @FunctionName("HttpExample")
+    @FunctionName("Janken")
     public HttpResponseMessage run(
             @HttpTrigger(
                 name = "req",
-                methods = {HttpMethod.GET, HttpMethod.POST},
+                methods = {HttpMethod.POST},
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+        final String move = request.getBody().orElse(null);
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        if (move == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
+                    .body("Please pass a valid move in the request body: rock, scissors, or paper.")
+                    .build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            String result;
+            switch (move) {
+                case "rock":
+                    result = "scissors";
+                    break;
+                case "scissors":
+                    result = "paper";
+                    break;
+                case "paper":
+                    result = "rock";
+                    break;
+                default:
+                    result = "Invalid move";
+                    break;
+            }
+
+            return request.createResponseBuilder(HttpStatus.OK)
+                    .body(result)
+                    .build();
         }
     }
 }
